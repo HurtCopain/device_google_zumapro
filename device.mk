f997625fac49576d1775bc8fa71d30bd803e9f48
Kyle Zhang <kelzhan@google.com>
2023-04-21 17:42:50 +0000

    Move Widevine drm to gs-common
    Bug: 277764729
    Change-Id: I5ca57bc83b98204a2efe7d382b7900197ae1640a

--- device.mk
+++ device.mk
@@ -33,6 +33,7 @@ include device/google/gs-common/gxp/dump.mk
 include device/google/gs-common/camera/dump.mk
 include device/google/gs-common/radio/dump.mk
 include device/google/gs-common/gear/dumpstate/aidl.mk
+include device/google/gs-common/widevine/widevine.mk
 
 include device/google/zumapro/dumpstate/item.mk
 
