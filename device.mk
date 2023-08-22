701d5907136831e6634a16a44a44af74e24b2ba0
Klines Jiang <klinesjiang@google.com>
2023-08-22 03:13:34 +0000

    [SELinux] Add gyotaku_app doamin to Pixel 2023.
    Bug: 296836878
    
    Test: Local build and tested pass.
    Change-Id: Ie996586ea19ce1b0b700a0192b613d2769dd0291

--- device.mk
+++ device.mk
@@ -37,6 +37,7 @@ include device/google/gs-common/gear/dumpstate/aidl.mk
 include device/google/gs-common/widevine/widevine.mk
 include device/google/gs-common/sota_app/factoryota.mk
 include device/google/gs-common/misc_writer/misc_writer.mk
+include device/google/gs-common/gyotaku_app/gyotaku.mk
 
 include device/google/zumapro/dumpstate/item.mk
 
